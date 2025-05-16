851be3cbfc3e66160b05b445d3a780164056586c
samuelhgp <165844544+samuelhgp@users.noreply.github.com>
2025-05-16 17:52:06 -0500

    hola

---
+++ src/__test__/index.test.js
@@ -0,0 +1,15 @@
+newFunction();
+
+function newFunction() {
+    const sum = require("../index");
+
+    describe("sum", () => {
+        test("should add two numbers correctly", () => {
+            expect(sum(1, 2)).toBe(3);
+        });
+
+        test("should handle negative numbers", () => {
+            expect(sum(-5, 3)).toBe(-2);
+        });
+    });
+}
